aa72311745583956b877884de514f4ae220c4add
Juan Castañeda <jncastt@gmail.com>
2019-04-17 13:01:05 -0500

    Se creó la Vista y Modelo del CRUD del apartado clientes.
    Del escritorio.

---
+++ desktop/Cliente/src/Modelo/Contacto.java
@@ -0,0 +1,45 @@
+
+package Modelo;
+
+public class Contacto {
+
+    private String id;
+    private String nombreServicio; 
+    private String emailContacto; 
+    private String telefono; 
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombreServicio() {
+        return nombreServicio;
+    }
+
+    public String getEmailContacto() {
+        return emailContacto;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNombreServicio(String nombreServicio) {
+        this.nombreServicio = nombreServicio;
+    }
+
+    public void setEmailContacto(String emailContacto) {
+        this.emailContacto = emailContacto;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
+}
